9cb3f7fa33115993d5878ec6b3da335bd1e55bc8
ayaelhenawy <elhenawyaya212gmail.com>
2024-09-23 05:00:13 +0300

    global error handling

--- index.js
+++ index.js
@@ -13,4 +13,12 @@ app.use(msgRouter)
 
 
 dbConnections();
+
+
+app.use((err,req,res,next)=>{
+    res.json({error:err});
+})
+// العمده
+// دي  middleware  تلاقي هتيجي لل   error  اللي جواها اول متشوف next  ال  catchError  هنا وحدت الريسبونس ال ال فانكشن اللي اسمها    
+
 app.listen(port, () => console.log(`server is running... `))
