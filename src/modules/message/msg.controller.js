9932e8f8a0ee440f7e6d3fa1aba34a749f71ef03
ayaelhenawy <elhenawyaya212gmail.com>
2024-09-27 00:33:53 +0300

    shareProfile use QR Code

--- src/modules/message/msg.controller.js
+++ src/modules/message/msg.controller.js
@@ -1,12 +1,11 @@
 import { msgModel } from "../../../Database/models/message.model.js"
 import { catchError } from "../../middleware/catchGlobalError.js";
-
+import QRcode from 'qrcode'
 
 
 const userMsg=catchError(async(req,res)=>{
     let msgs=await msgModel.find({recievedId:req.user.userId});  
      //لتزم نفس الاسم ال الريكويست اللي موجود ف ال ميديل وير اللي قبلها 
-    
     res.json({message:"success",msgs});
 })
 
@@ -14,7 +13,12 @@ const addMsg=catchError(async(req,res)=>{
     await msgModel.insertMany(req.body);
     res.json({message:"success"});
 })
+const shareProfile=catchError(async(req,res)=>{
+    QRcode.toDataURL('http://localhost:3000/messages',(err,qr)=>{
+    res.send(`<img src=${qr}>`)
+    })
+})
 
 export{
-    userMsg,addMsg
+    userMsg,addMsg,shareProfile
 }
--- src/modules/message/msg.router.js
+++ src/modules/message/msg.router.js
@@ -1,6 +1,6 @@
 
 import express from 'express'
-import { addMsg, userMsg } from './msg.controller.js';
+import { addMsg, shareProfile, userMsg } from './msg.controller.js';
 import { auth } from '../../middleware/auth.js';
 
 
@@ -9,6 +9,7 @@ import { auth } from '../../middleware/auth.js';
 const msgRouter= express.Router();
 msgRouter.get('/messages',auth,userMsg)
 msgRouter.post('/messages',auth,addMsg)
+msgRouter.get('/shareProfile',shareProfile)
 
 
 export{
